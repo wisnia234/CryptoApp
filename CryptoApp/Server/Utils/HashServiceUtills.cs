9d7976da727ab7cb8f82cc989781d8f1ea7667a7
HP_OMEN\micha <michalw65@gmail.com>
2023-10-29 20:40:47 +0100

    CreateHashAlgorithm function refactor

--- CryptoApp/Server/Utils/HashServiceUtills.cs
+++ CryptoApp/Server/Utils/HashServiceUtills.cs
@@ -4,16 +4,13 @@ namespace CryptoApp.Server.Utils;
 
 internal static class HashServiceUtills
 {
-    public static HashAlgorithm CreateHashAlgorithm(string AlgorithmName)
+    public static HashAlgorithm CreateHashAlgorithm(string AlgorithmName) => AlgorithmName switch
     {
-        return AlgorithmName switch
-        {
-            "MD5" => MD5.Create(),
-            "SHA1" => SHA1.Create(),
-            "SHA256" => SHA256.Create(),
-            "SHA384" => SHA384.Create(),
-            "SHA512" => SHA512.Create(),
-            _ => throw new ArgumentException("Nieznana nazwa algorytmu skrótu")
-        };
-    }
+        "MD5" => MD5.Create(),
+        "SHA1" => SHA1.Create(),
+        "SHA256" => SHA256.Create(),
+        "SHA384" => SHA384.Create(),
+        "SHA512" => SHA512.Create(),
+        _ => throw new ArgumentException("Nieznana nazwa algorytmu skrótu")
+    };
 }
