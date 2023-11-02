d2fc811f73d8311e0804d5022d5b772b80ed3af1
HP_OMEN\micha <michalw65@gmail.com>
2023-11-02 16:00:58 +0100

    Added HMAC authentication to Encryption Service

--- CryptoApp/Client/Models/EncryptionModel/EncryptionOptions.cs
+++ CryptoApp/Client/Models/EncryptionModel/EncryptionOptions.cs
@@ -3,7 +3,6 @@
 internal static class EncryptionOptions
 {
     public static List<string> AvailableEncryptionAlgorithms => new() { "AES", "DES", "3DES" };
-    public static List<string> AvailableCipherModes => new() { "CBC", "ECB", "CFB" };
-    public static List<int> AvailableKeySizes => new() { 128, 256 };
+    public static List<string> AvailableCipherModes => new() { "CFB", "CBC", "ECB" };
 
 }
--- CryptoApp/Server/Services/EncryptionService.cs
+++ CryptoApp/Server/Services/EncryptionService.cs
@@ -7,9 +7,121 @@ namespace CryptoApp.Server.Services;
 
 internal class EncryptionService : IEncryptionService
 {
-    public string SymetricAlgorithmOperation(BaseEncryptionCommand command)
+    public string SymetricAlgorithmOperation(BaseEncryptionCommand command) => command switch
     {
-        bool isEncryption = command is EncryptionCommand;
+        EncryptionCommand => EncryptData(command),
+        DecryptionCommand => DecryptData(command),
+        _ => throw new NotImplementedException() 
+    };
+
+     private string EncryptData(BaseEncryptionCommand command)
+     {
+        byte[] salt = CryptoServiceUtills.GenerateSaltBytes(128);
+        byte[] plainBytes = command.ContentData;
+
+        using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(command.Password, salt, 100_000, HashAlgorithmName.SHA256);
+        using SymmetricAlgorithm symmetricAlgorithm = EncryptionServiceUtils.CreateSymmetricAlgorithm(command.Algorithm);
+
+        if (command.Algorithm.Equals("AES"))
+        {
+            symmetricAlgorithm.KeySize = 256;
+        }
+        symmetricAlgorithm.Key = pdb.GetBytes(symmetricAlgorithm.KeySize / 8);
+        symmetricAlgorithm.GenerateIV();
+        symmetricAlgorithm.Mode = EncryptionServiceUtils.GetCipherModeFromString(command.CipherMode);
+        symmetricAlgorithm.Padding = PaddingMode.PKCS7;
+
+        
+        using MemoryStream ms = new MemoryStream();
+        using CryptoStream cs = new CryptoStream(ms, symmetricAlgorithm.CreateEncryptor(), CryptoStreamMode.Write);
+        cs.Write(plainBytes, 0, plainBytes.Length);
+        cs.FlushFinalBlock();
+        byte[] cipherBytes = ms.ToArray();
+
+        HMACSHA512 hmac = new(symmetricAlgorithm.Key);
+        byte[] saltedCipherBytes = new byte[salt.Length + symmetricAlgorithm.IV.Length + cipherBytes.Length];
+
+        Array.Copy(salt, 0, saltedCipherBytes, 0, salt.Length);
+        Array.Copy(symmetricAlgorithm.IV, 0, saltedCipherBytes, salt.Length, symmetricAlgorithm.IV.Length);
+        Array.Copy(cipherBytes, 0, saltedCipherBytes, symmetricAlgorithm.IV.Length + salt.Length, cipherBytes.Length);
+
+
+        byte[] hmacHash = hmac.ComputeHash(saltedCipherBytes);
+        byte[] resultWithHMAC = new byte[saltedCipherBytes.Length + hmacHash.Length];
+
+        Array.Copy(saltedCipherBytes, 0, resultWithHMAC, 0, saltedCipherBytes.Length);
+        Array.Copy(hmacHash, 0, resultWithHMAC, saltedCipherBytes.Length, hmacHash.Length);
+        symmetricAlgorithm.Clear();
+
+        return Convert.ToBase64String(resultWithHMAC);
+     }
+
+    private string DecryptData(BaseEncryptionCommand command)
+    {
+        byte[] resultWithHmac = command.ContentData;
+        byte[] hmacHash = new byte[64];
+
+        Array.Copy(resultWithHmac, resultWithHmac.Length - 64, hmacHash, 0, hmacHash.Length);
+        byte[] saltedCipherBytes = new byte[resultWithHmac.Length - 64];
+        Array.Copy(resultWithHmac, 0, saltedCipherBytes, 0, resultWithHmac.Length - 64);
+
+
+        byte[] salt = new byte[128];
+        byte[] iv = new byte[EncryptionServiceUtils.GetSymetricAlgorithmIVSize(command.Algorithm)];
+        byte[] cipherBytes = new byte[saltedCipherBytes.Length - iv.Length - salt.Length];
+
+        Array.Copy(saltedCipherBytes, 0, salt, 0, salt.Length);
+        Array.Copy(saltedCipherBytes, salt.Length, iv, 0, iv.Length);
+        Array.Copy(saltedCipherBytes, salt.Length + iv.Length, cipherBytes, 0, saltedCipherBytes.Length - salt.Length - iv.Length);
+
+        using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(command.Password, salt, 100_000, HashAlgorithmName.SHA256);
+        using SymmetricAlgorithm symmetricAlgorithm = EncryptionServiceUtils.CreateSymmetricAlgorithm(command.Algorithm);
+
+        if (command.Algorithm.Equals("AES"))
+        {
+            symmetricAlgorithm.KeySize = 256;
+        }
+        symmetricAlgorithm.Key = pdb.GetBytes(symmetricAlgorithm.KeySize / 8);
+        symmetricAlgorithm.IV = iv;
+        symmetricAlgorithm.Padding = PaddingMode.PKCS7;
+        symmetricAlgorithm.Mode = EncryptionServiceUtils.GetCipherModeFromString(command.CipherMode);
+
+        HMACSHA512 hMACSHA512 = new(symmetricAlgorithm.Key);
+        byte[] messageHash = hMACSHA512.ComputeHash(saltedCipherBytes);
+        if (!HashCompare(hmacHash, messageHash))
+        {
+            throw new Exception("Wrong password");
+        }
+
+        byte[] result;
+        try
+        {
+            using MemoryStream ms = new MemoryStream();
+            using CryptoStream cs = new CryptoStream(ms, symmetricAlgorithm.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(cipherBytes, 0, cipherBytes.Length);
+            cs.FlushFinalBlock();
+            result = ms.ToArray();
+            symmetricAlgorithm.Clear();
+        }
+        catch(CryptographicException)
+        {
+            throw new Exception("Error, please check if you provided correct algorithm o cipher mode");
+        }
+
+        return Convert.ToBase64String(result);
+    }
+
+    private bool HashCompare(byte[] hash1, byte[] hash2) => hash1.SequenceEqual(hash2);
+
+
+}
+
+
+
+
+
+
+/*bool isEncryption = command is EncryptionCommand;
         byte[] salt;
         byte[] saltedCipherBytes;
         byte[] iv = new byte[EncryptionServiceUtils.GetSymetricAlgorithmIVSize(command.Algorithm)];
@@ -80,7 +192,4 @@ internal class EncryptionService : IEncryptionService
 
         symmetricAlgorithm.Clear();
 
-        return Convert.ToBase64String(result);
-    }
-
-}
+        return Convert.ToBase64String(result);*/
