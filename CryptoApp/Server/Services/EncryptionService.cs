74b0adbf892181181f736b7b4df4b9c0c3bff064
HP_OMEN\micha <michalw65@gmail.com>
2023-11-02 17:03:35 +0100

    Added password visibility option

--- CryptoApp/Server/Services/EncryptionService.cs
+++ CryptoApp/Server/Services/EncryptionService.cs
@@ -73,7 +73,6 @@ internal class EncryptionService : IEncryptionService
         Array.Copy(saltedCipherBytes, 0, salt, 0, salt.Length);
         Array.Copy(saltedCipherBytes, salt.Length, iv, 0, iv.Length);
         Array.Copy(saltedCipherBytes, salt.Length + iv.Length, cipherBytes, 0, saltedCipherBytes.Length - salt.Length - iv.Length);
-
         using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(command.Password, salt, 100_000, HashAlgorithmName.SHA256);
         using SymmetricAlgorithm symmetricAlgorithm = EncryptionServiceUtils.CreateSymmetricAlgorithm(command.Algorithm);
 
@@ -92,7 +91,6 @@ internal class EncryptionService : IEncryptionService
         {
             throw new Exception("Wrong password");
         }
-
         byte[] result;
         try
         {
@@ -115,81 +113,3 @@ internal class EncryptionService : IEncryptionService
 
 
 }
-
-
-
-
-
-
-/*bool isEncryption = command is EncryptionCommand;
-        byte[] salt;
-        byte[] saltedCipherBytes;
-        byte[] iv = new byte[EncryptionServiceUtils.GetSymetricAlgorithmIVSize(command.Algorithm)];
-        byte[] bytesToProcess;
-
-        if (isEncryption)
-        {
-            salt = CryptoServiceUtills.GenerateSaltBytes(128);
-            bytesToProcess = command.ContentData;
-        }
-        else
-        {
-            saltedCipherBytes = command.ContentData;
-            salt = new byte[128];
-            bytesToProcess = new byte[saltedCipherBytes.Length - iv.Length - salt.Length];
-
-            Array.Copy(saltedCipherBytes, 0, salt, 0, salt.Length);
-            Array.Copy(saltedCipherBytes, salt.Length, iv, 0, iv.Length);
-            Array.Copy(saltedCipherBytes, salt.Length + iv.Length, bytesToProcess, 0, saltedCipherBytes.Length - salt.Length - iv.Length);
-        }
-
-        using Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(command.Password, salt, 100_000, HashAlgorithmName.SHA256);
-        using SymmetricAlgorithm symmetricAlgorithm = EncryptionServiceUtils.CreateSymmetricAlgorithm(command.Algorithm);
-
-        if (command.Algorithm.Equals("AES"))
-        {
-            symmetricAlgorithm.KeySize = 256;
-        }
-        symmetricAlgorithm.Key = pdb.GetBytes(symmetricAlgorithm.KeySize / 8);
-
-        if (isEncryption)
-        {
-            symmetricAlgorithm.GenerateIV();
-        }
-        else
-        {
-            symmetricAlgorithm.IV = iv;
-        }
-        symmetricAlgorithm.Padding = PaddingMode.PKCS7;
-        symmetricAlgorithm.Mode = EncryptionServiceUtils.GetCipherModeFromString(command.CipherMode);
-
-        ICryptoTransform cryptoTransform = isEncryption ? symmetricAlgorithm.CreateEncryptor() : symmetricAlgorithm.CreateDecryptor();
-        byte[] result;
-
-        try
-        {
-            using (MemoryStream ms = new MemoryStream())
-            using (CryptoStream cs = new CryptoStream(ms, cryptoTransform, CryptoStreamMode.Write))
-            {
-                cs.Write(bytesToProcess, 0, bytesToProcess.Length);
-                cs.FlushFinalBlock();
-                result = ms.ToArray();
-            }
-        }
-        catch (CryptographicException) 
-        {
-            throw new Exception("Wrong password or parameters were provided");
-        }
-
-        if (isEncryption)
-        {
-            byte[] combinedBytes = new byte[salt.Length + symmetricAlgorithm.IV.Length + result.Length];
-            Array.Copy(salt, 0, combinedBytes, 0, salt.Length);
-            Array.Copy(symmetricAlgorithm.IV, 0, combinedBytes, salt.Length, symmetricAlgorithm.IV.Length);
-            Array.Copy(result, 0, combinedBytes, symmetricAlgorithm.IV.Length + salt.Length, result.Length);
-            result = combinedBytes;
-        }
-
-        symmetricAlgorithm.Clear();
-
-        return Convert.ToBase64String(result);*/
