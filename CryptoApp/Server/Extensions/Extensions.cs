27b9b6d4fceadde0747898789cf1e9ee1a88f38d
HP_OMEN\micha <michalw65@gmail.com>
2023-11-04 17:49:45 +0100

    Added extension to program.cs

--- CryptoApp/Server/Extensions/Extensions.cs
+++ CryptoApp/Server/Extensions/Extensions.cs
@@ -1,13 +1,17 @@
-﻿using CryptoApp.Server.Middleware;
+﻿using CryptoApp.Server.Data;
+using CryptoApp.Server.Middleware;
+using CryptoApp.Server.Models;
 using CryptoApp.Server.Services;
 using CryptoApp.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 
 namespace CryptoApp.Server.Extensions;
 
 internal static class Extensions
 {
-    public static IServiceCollection AddCryptoServices(this IServiceCollection services)
+    private static IServiceCollection AddCryptoServices(this IServiceCollection services)
     {
         services.AddScoped<IEncryptionService, EncryptionService>();
         services.AddScoped<ISignatureService, SignatureService>();
@@ -16,19 +20,58 @@ internal static class Extensions
         services.AddScoped<IHashService, HashService>();
         return services;
     }
+
+    public static IServiceCollection AddServerExtensions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCryptoServices();
+        services.AddDbAndIdentity(configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddDbAndIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        services.AddDefaultIdentity<ApplicationUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = false;
+            options.Password.RequiredLength = 10;
+        })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddIdentityServer()
+            .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        services.AddAuthentication()
+            .AddIdentityServerJwt();
+
+        services.AddControllersWithViews();
+        services.AddRazorPages();
+        return services;
+    }
     public static WebApplication AddWebApplication(this WebApplication app)
     {
         app.UseMiddleware<ExceptionMiddleware>();
         app.UseHttpsRedirection();
+
         app.UseBlazorFrameworkFiles();
         app.UseStaticFiles();
 
         app.UseRouting();
 
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
 
         app.MapRazorPages();
         app.MapControllers();
         app.MapFallbackToFile("index.html");
+
         return app;
     }
     public static string ToEnglishMessage(this Exception ex)
--- CryptoApp/Server/Program.cs
+++ CryptoApp/Server/Program.cs
@@ -1,36 +1,9 @@
-using CryptoApp.Server.Data;
 using CryptoApp.Server.Extensions;
-using CryptoApp.Server.Middleware;
-using CryptoApp.Server.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddCryptoServices();
-builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-    options.User.RequireUniqueEmail = false;
-    options.Password.RequiredLength = 10;
-})
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddServerExtensions(builder.Configuration);
 
 var app = builder.Build();
 
@@ -46,20 +19,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseHttpsRedirection();
 
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseIdentityServer();
-app.UseAuthorization();
-
-
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.AddWebApplication();
 
 app.Run();
